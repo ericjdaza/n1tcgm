d0a63102409d0058cbfaba29c6ce773e69a8c4fc
Eric J. Daza <ericjdaza@gmail.com>
2019-04-15 17:18:47 -0700

    Update p171106m.R

--- R/scripts/p171106m.R
+++ R/scripts/p171106m.R
@@ -16,6 +16,7 @@ rm(list=ls())
 # 171107t [Daza_EJ] --- Updated while working on manuscript.
 # 181113t --- Produced output and visualizations for Kate's
 #   use in her 20 Nov 2018 job talk.
+# 190126s --- Added in PANAS scoring.
 #
 
 
@@ -58,21 +59,18 @@ scalar.seed <- 1711061706
 
 
 ### Path and variable names: Define.
-programpath <- "Dropbox (Personal)/Professional/My Documents/academic/postdocs/SPRC/Projects/n1ci/n1t.cgm"
-# ericname <- "edaza"
-ericname <- "ericjdaza"
-path.ejdtools <- paste0("/Users/", ericname, "/Dropbox (Personal)/Professional/My Documents/Programming/R/packages/ejdtools/")
-path.wd <- paste0("/Users/", ericname, "/", programpath, "/programming")
-path.sourceinfo <- paste0(path.wd, "/sourceinfo/")
-path.output <- paste0(path.wd, "/output/R/p171106m/")
-path.programs <- paste0(path.wd, "/programs/R/p171106m/")
+# path_ejdtools <- paste0("/Users/ericjdaza/Dropbox (Personal)/Professional/My Documents/Programming/R/packages/ejdtools/")
+path_wd <- "/Users/ericjdaza/Documents/GitHub/casia/n1tcgm/"
+path_R <- paste0(path_wd, "R/")
+path_data_source <- paste0(path_wd, "data/source/")
+path_data_output <- paste0(path_wd, "data/output/")
 
 
 
 ### External user-defined functions.
 
 # Define function to construct variable-characteristics codebook.
-# if(!exists("foo", mode="function")) source(paste0(path.ejdtools, "ejdtools_charvar.R"))
+# if(!exists("foo", mode="function")) source(paste0(path_ejdtools, "ejdtools_charvar.R"))
 
 
 
@@ -103,7 +101,7 @@ path.programs <- paste0(path.wd, "/programs/R/p171106m/")
 ### Questionnaire survey (i.e., Qualtrics) data.
 df_qualtrics <- openxlsx::read.xlsx(
   xlsxFile = paste0(
-    path.sourceinfo,
+    path_data_source,
     "N of 1 daily cravings and mood_September 25, 2017_11.39_ejdedits.xlsx"
   ),
   colNames = TRUE,
@@ -114,6 +112,48 @@ df_qualtrics <- openxlsx::read.xlsx(
 df_qualtrics$Start.Date <- as.POSIXct((df_qualtrics$Start.Date)*86400, tz="GMT", origin="1899-12-30")
 df_qualtrics$End.Date <- as.POSIXct((df_qualtrics$End.Date)*86400, tz="GMT", origin="1899-12-30")
 df_qualtrics$Recorded.Date <- as.POSIXct((df_qualtrics$Recorded.Date)*86400, tz="GMT", origin="1899-12-30")
+
+# Construct PANAS (Positive Affect / Negative Affect Score).
+for (name in names(df_qualtrics)) {
+  if (
+    regexpr(
+      pattern = "Below.is.a.list.of.words.that.describe.feelings.that.people.have..Please.read.each.word.carefully..Then.click.the.circle.that.best.describes.how.you.feel.right.now..-.",
+      text = names(df_qualtrics)[which(names(df_qualtrics) == name)]
+    ) == 1
+  ) names(df_qualtrics)[which(names(df_qualtrics) == name)] <- gsub(
+    pattern = "Below.is.a.list.of.words.that.describe.feelings.that.people.have..Please.read.each.word.carefully..Then.click.the.circle.that.best.describes.how.you.feel.right.now..-.",
+    replacement = "",
+    x = names(df_qualtrics)[which(names(df_qualtrics) == name)]
+  )
+  
+}
+rm(name)
+names(df_qualtrics)
+df_qualtrics <- df_qualtrics %>%
+  dplyr::mutate(
+    posaff_score = Interested +
+      Excited +
+      Strong +
+      Enthusiastic +
+      Proud +
+      Alert +
+      Inspired +
+      Determined +
+      Attentive +
+      Active,
+    negaff_score = Distressed +
+      Upset +
+      Guilty +
+      Scared +
+      Hostile +
+      Irritable +
+      Ashamed +
+      Nervous +
+      Jittery +
+      Afraid
+  )
+summary(df_qualtrics$posaff_score)
+summary(df_qualtrics$negaff_score)
 
 
 ## Participant: Katarzyna Wac.
@@ -131,7 +171,7 @@ df_qualtrics_daza <- df_qualtrics[which(df_qualtrics$`Are.you.Kate.or.Eric?`==2)
 ## Participant: Katarzyna Wac.
 df_cgm_wac <- read.xlsx(
   xlsxFile = paste0(
-    path.sourceinfo,
+    path_data_source,
     "wac_k/FreestyleLibre_Glucose.xlsx"
   ),
   colNames = FALSE,
@@ -176,46 +216,28 @@ tbl_wac <- dplyr::as_tibble(df_cgm_wac) %>%
   ) %>%
   dplyr::arrange(datetime) %>%
   dplyr::mutate(
+    monthday = strftime(
+      x = datetime,
+      format = "%m-%d",
+      tz="GMT",
+      origin="1899-12-30"
+    ),
     treatment = ifelse(
-      strftime(
-        x = datetime,
-        format = "%m-%d",
-        tz="GMT",
-        origin="1899-12-30"
-      ) %in%
-        c(
-          "07-30",
-          "08-01"
-        ),
+      monthday %in% c(
+        "07-30",
+        "08-01"
+      ),
       "pre",
       ifelse(
-        strftime(
-          x = datetime,
-          format = "%m-%d",
-          tz="GMT",
-          origin="1899-12-30"
-        ) == "08-26",
+        monthday == "08-26",
         "post",
         ifelse(
-          strftime(
-            x = datetime,
-            format = "%m-%d",
-            tz="GMT",
-            origin="1899-12-30"
-          ) %in%
+          monthday %in%
             c(
-              "08-02",
-              "08-03",
-              "08-04",
-              "08-11",
-              "08-12",
-              "08-13",
-              "08-17",
-              "08-18",
-              "08-19",
-              "08-23",
-              "08-24",
-              "08-25"
+              "08-02", "08-03", "08-04",
+              "08-11", "08-12", "08-13",
+              "08-17", "08-18", "08-19",
+              "08-23", "08-24", "08-25"
             ),
           "B",
           "A"
@@ -225,116 +247,24 @@ tbl_wac <- dplyr::as_tibble(df_cgm_wac) %>%
   ) %>%
   dplyr::rename(dv_craving = `Craving.is.an.intense.desire.to.consume.a.particular.food.or.food.type.that.is.difficulty.to.resist..Do.you.feel.craving.for.a.food.or.food.type.right.now?.(If.you.just.ate,.answer.the.question.for.did.you.feel.craving.before.you.ate.)`)
 tbl_wac$period <- NA
-tbl_wac$period[tbl_wac$treatment == "pre"] <- 0
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-02",
-      "08-03",
-      "08-04"
-    )
-  ] <- 1
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-05",
-      "08-06",
-      "08-07"
-    )
-] <- 2
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-08",
-      "08-09",
-      "08-10"
-    )
-  ] <- 3
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-11",
-      "08-12",
-      "08-13"
-    )
-  ] <- 4
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-14",
-      "08-15",
-      "08-16"
-    )
-  ] <- 5
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-17",
-      "08-18",
-      "08-19"
-    )
-  ] <- 6
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-20",
-      "08-21",
-      "08-22"
-    )
-  ] <- 7
-tbl_wac$period[
-  strftime(
-    x = tbl_wac$datetime,
-    format = "%m-%d",
-    tz="GMT",
-    origin="1899-12-30"
-  ) %in%
-    c(
-      "08-23",
-      "08-24",
-      "08-25"
-    )
-  ] <- 8
-tbl_wac$period[tbl_wac$treatment == "post"] <- 999
+for (i in 1:(nrow(tbl_wac))) {
+  
+  if (tbl_wac$treatment == "pre") tbl_wac$period <- 0
+  if (tbl_wac$monthday %in% c("08-02", "08-03", "08-04")) tbl_wac$period <- 1
+  if (tbl_wac$monthday %in% c("08-05", "08-06", "08-07")) tbl_wac$period <- 2
+  if (tbl_wac$monthday %in% c("08-08", "08-09", "08-10")) tbl_wac$period <- 3
+  if (tbl_wac$monthday %in% c("08-11", "08-12", "08-13")) tbl_wac$period <- 4
+  if (tbl_wac$monthday %in% c("08-14", "08-15", "08-16")) tbl_wac$period <- 5
+  if (tbl_wac$monthday %in% c("08-17", "08-18", "08-19")) tbl_wac$period <- 6
+  if (tbl_wac$monthday %in% c("08-20", "08-21", "08-22")) tbl_wac$period <- 7
+  if (tbl_wac$monthday %in% c("08-23", "08-24", "08-25")) tbl_wac$period <- 8
+  if (tbl_wac$treatment == "post") tbl_wac$period <- 999
+  
+}
+rm(i)
 tbl_wac <- tbl_wac %>%
   dplyr::group_by(period) %>%
-  dplyr::mutate(timepoint_index = row_number())
-tbl_wac <- tbl_wac %>%
+  dplyr::mutate(timepoint_index = row_number()) %>%
   dplyr::filter(timepoint_index == 1) %>%
   dplyr::select(
     period,
@@ -357,17 +287,10 @@ tbl_wac_studyperiod <- tbl_wac %>%
 
 
 # # Save image.
-# save.image(paste0(path.output, "<filename>.RData"))
+# save.image(paste0(path_data_output, "<filename>.RData"))
 
 # # Load image.
-# load(paste0(path.output, "<filename>.RData"))
-# ericname <- "edaza"
-# # ericname <- "ericjdaza"
-# path.ejdtools <- paste0("/Users/", ericname, "/Dropbox (Personal)/Professional/My Documents/Programming/R/packages/ejdtools/")
-# path.wd <- paste0("/Users/", ericname, "/", programpath, "/programming")
-# path.sourceinfo <- paste0(path.wd, "/sourceinfo/")
-# path.output <- paste0(path.wd, "/output/R/p171106m/")
-# path.programs <- paste0(path.wd, "/programs/R/p171106m/")
+# load(paste0(path_data_output, "<filename>.RData"))
 
 
 
@@ -380,9 +303,9 @@ tbl_wac_studyperiod <- tbl_wac %>%
 ### Treatment data: Create from assigned treatments.
 
 
-## Participant: Katarzyna Wac.
-datetime.min.wac <- min()
-  
+# ## Participant: Katarzyna Wac.
+# datetime.min.wac <- min()
+
 
 ## Participant: Eric J. Daza.
 
@@ -423,11 +346,25 @@ datetime.min.wac <- min()
 # ===================== #
 
 
-## Scatterplots.
+
+
+
+##### Tables.
+
+## BGL: We have enough data to model and test with CAPTEuR.
+
+
+
+
+
+##### Figures.
 xlim_touse <- c(
   min(tbl_wac_studyperiod$timepoint),
   max(tbl_wac_studyperiod$timepoint)
 )
+
+
+## Blood glucose level.
 ylim_touse <- c(
   min(log(tbl_wac_studyperiod$bg.level), na.rm = TRUE),
   max(log(tbl_wac_studyperiod$bg.level), na.rm = TRUE)
@@ -445,7 +382,7 @@ tbl_wac_forplot <- tbl_wac_studyperiod %>%
     )
   )
 set.seed(scalar.seed)
-ggp_scatter <- ggplot2::ggplot(
+ggp_bglevel <- ggplot2::ggplot(
   data = tbl_wac_forplot,
   aes(
     x = timepoint,
@@ -460,23 +397,21 @@ ggp_scatter <- ggplot2::ggplot(
     ),
     alpha = global_alpha
   ) +
-  ggplot2::geom_jitter(
+  ggplot2::geom_point(
     aes(
       x = timepoint,
       y = dv_craving_scaled,
       # group = period,
       color = treatment
     ),
-    width = 0.11,
-    height = 0.22,
     size = 5,
     alpha = global_alpha
   ) +
   ggplot2::xlim(xlim_touse)
-ggp_scatter
+ggp_bglevel
 
 
-## Jitter plot.
+## Craving.
 set.seed(scalar.seed)
 ggp_jitter <- ggplot2::ggplot(
   data = tbl_wac_forplot,
@@ -494,6 +429,59 @@ ggp_jitter <- ggplot2::ggplot(
     alpha = global_alpha
   )
 ggp_jitter
+
+
+## PANAS.
+ylim_touse <- c(0, 50)
+tbl_wac_forplot_panas <- tbl_wac_studyperiod %>%
+  dplyr::mutate(
+    dv_craving_scaled = ifelse(
+      dv_craving == 1,
+      ylim_touse[1],
+      ifelse(
+        dv_craving == 2,
+        ylim_touse[2],
+        NA
+      )
+    )
+  )
+set.seed(scalar.seed)
+ggp_panas <- ggplot2::ggplot(
+  data = tbl_wac_forplot_panas,
+  aes(
+    x = timepoint,
+    y = posaff_score
+  )
+) +
+  ggplot2::theme_classic() +
+  ggplot2::geom_point(
+    aes(
+      group = period,
+      color = treatment
+    ),
+    size = 3,
+    alpha = global_alpha
+  ) +
+  ggplot2::geom_point(
+    aes(
+      y = negaff_score,
+      group = period,
+      color = treatment
+    ),
+    alpha = global_alpha
+  ) +
+  ggplot2::geom_point(
+    aes(
+      x = timepoint,
+      y = dv_craving_scaled,
+      # group = period,
+      color = treatment
+    ),
+    size = 5,
+    alpha = global_alpha
+  ) +
+  ggplot2::xlim(xlim_touse)
+ggp_panas
 
 
 
